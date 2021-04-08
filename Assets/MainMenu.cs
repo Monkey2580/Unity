03b0694866fc7ea3e9767867576771708413bd35
rakaprakoso <rakaprakoso2@gmail.com>
2021-04-08 18:17:06 +0700

    Test Commit

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,6 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    ##Test
 
     public void QuitGame() {
         Debug.Log("Quit The Game");
